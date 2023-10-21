d95d557f7cc5fc8d4b779d2d0b96c2903951507e
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-21 07:37:12 -0700

    rhf error

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,24 +1,50 @@
 import { useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
-let renderCount = 0;
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
 
 const YouTubeForm = () => {
-  const form = useForm();
+  const form = useForm<FormValues>();
 
-  const { register, control } = form;
+  const { register, control, handleSubmit } = form;
 
-  renderCount++;
+  const onSubmit = (data: FormValues) => {
+    console.log('form submitted', data);
+  };
 
   return (
     <div>
-      <h1>YouTube Form {renderCount / 2}</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' {...register('username')} />
+        <input
+          type='text'
+          id='username'
+          {...(register('username'),
+          {
+            required: {
+              value: true,
+              message: 'Username is required',
+            },
+          })}
+        />
 
         <label htmlFor='email'>Email</label>
-        <input type='email' id='email' {...register('email')} />
+        <input
+          type='email'
+          id='email'
+          {...(register('email'),
+          {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: 'Invalid email format',
+            },
+          })}
+        />
 
         <label htmlFor='channel'>Channel</label>
         <input type='text' id='channel' {...register('channel')} />
