479ba5b4fd69e845a6e66d73020724b4766a84bd
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-21 06:40:46 -0700

    through video 6

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,33 @@
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
+let renderCount = 0;
+
+const YouTubeForm = () => {
+  const form = useForm();
+
+  const { register, control } = form;
+
+  renderCount++;
+
+  return (
+    <div>
+      <h1>YouTube Form {renderCount / 2}</h1>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' {...register('username')} />
+
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' {...register('email')} />
+
+        <label htmlFor='channel'>Channel</label>
+        <input type='text' id='channel' {...register('channel')} />
+
+        <input type='submit' />
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default YouTubeForm;
